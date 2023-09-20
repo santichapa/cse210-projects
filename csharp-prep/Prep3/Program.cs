454cd56bcda7193dd9ebd2cc8d9dcc50788621b2
santichapa <santi.chaparro280@gmail.com>
2023-09-20 12:55:42 -0300

    prep 3 tasks complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = false;
+
+        do
+        {
+            Console.WriteLine("Welcome to Guess the Number game, where you have to guess the magic number! Good luck... ");
+
+            Random randomNumGen = new Random();
+            int magicNum = randomNumGen.Next(1, 101);
+
+            bool guessLoop = true;
+            
+            do 
+            {
+                Console.Write("What is your guess? ");
+            int guessNum = int.Parse(Console.ReadLine());
+
+            if (magicNum < guessNum)
+            {
+                Console.WriteLine("Lower...");
+
+            }
+            else if (magicNum > guessNum)
+            {
+                Console.WriteLine("Higher...");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! You won!");
+                guessLoop = false;
+
+                Console.WriteLine("Do you want to play again? (yes/no) ");
+                string again = Console.ReadLine();
+                if (again == "yes")
+                {
+                    playAgain = true;
+                }
+                else {
+                    Console.WriteLine("Thank you for playing! See you later!");
+                    playAgain = false;
+                }
+                
+            }
+
+            }
+            while (guessLoop);
+        }
+        while (playAgain);
     }
 }
