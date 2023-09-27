4fd214f202a362dec619e77f64fb8392717bccb2
santichapa <santi.chaparro280@gmail.com>
2023-09-27 15:50:32 -0300

    finished learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Front-End Developer";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume resume1 = new Resume();
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1._name = "Allison Rose";
+
+        resume1.DisplayResume();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization.Metadata;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (var job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+
+}
