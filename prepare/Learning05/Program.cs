44a2c773194e0700b63cf35a9558420ce203aa36
santichapa <santi.chaparro280@gmail.com>
2023-11-06 16:43:47 -0300

    finished learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new("red", 5);
+        Rectangle rectangle = new("orange", 10, 5);
+        Circle circle = new("blue", 2.5);
+
+        List<Shape> shapes = new()
+        {
+            square,
+            rectangle,
+            circle
+        };
+
+        Console.WriteLine("Shapes:");
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}\tArea: {s.GetArea()}");
+            
+        }
+
+
+        
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,21 @@
+public class Shape
+{
+    protected string _color;
+
+public Shape(string color)
+{
+    _color = color;
+}
+
+    public void SetColor(string color) {
+        _color = color;
+    }
+    public string GetColor() {
+        return _color;
+    }
+    public virtual double GetArea()
+    {
+        return -1;
+    }
+
+}
