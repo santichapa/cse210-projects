96d1586a92b3c3eae23439e6fac0dc8eda0bd619
santichapa <santi.chaparro280@gmail.com>
2023-11-29 18:24:34 -0300

    added comments

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // create a video list
         List<Video> videos = new();
 
+        // create each video and add it to a list
         Video video1 = new("Mantis vs. Spider", "AwesomeNatureInsects", 452);
         Video video2 = new("Rain Ambience Music | 1 HOUR MIX", "Sounds&Beats", 3601);
         Video video3 = new("dog meme from social media", "RandomVids", 11);
@@ -16,6 +19,7 @@ class Program
         videos.Add(video3);
         videos.Add(video4);
 
+        // creating and adding comments to the videos
         video1.AddComment("user1302","Awesome video! never seen anything like it!");
         video1.AddComment("James_12234","I saw one of those in my backyard, they are really bigger than I expected!");
         video1.AddComment("Emily Notfake","I can't stand these videos, so gross!!");
@@ -33,7 +37,7 @@ class Program
         video4.AddComment("Don Smile","I never thought about it this way");
         video4.AddComment("AverageUser89","Great work @WorldSpeeches, this one is very good");
 
-
+        // display videos
         Console.WriteLine("--List of Videos--");
 
         foreach (Video v in videos)
