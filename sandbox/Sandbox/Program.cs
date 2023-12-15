1e3f223a7ee830f430e2b5dae2fe8000b50dcc35
santichapa <santi.chaparro280@gmail.com>
2023-12-15 00:39:46 -0300

    .

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        TextInfo myTI = new CultureInfo("en-US",false).TextInfo;
+
+        Console.WriteLine("---Epic Name Generator---\n\nWhat is your name? ");
+        string name = Console.ReadLine().ToLower();
+
+        List<char> nameAsList = name.ToList();
+
+        nameAsList.Sort();
+
+        string newName = "";
+
+        foreach (char c in nameAsList)
+        {
+            newName = $"{newName}{c}";
+        }        
+
+        Console.WriteLine($"\nYour new epic name is: {myTI.ToTitleCase(newName)}");
     }
 }
